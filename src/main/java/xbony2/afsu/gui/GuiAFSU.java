b0cdda407838e1cbcc1af29ec9411e57769d8b7d
xbony2 <xbony22@gmail.com>
2014-11-02 10:55:07 -0500

    Giving up for now...

--- src/main/java/xbony2/afsu/gui/GuiAFSU.java
+++ src/main/java/xbony2/afsu/gui/GuiAFSU.java
@@ -28,7 +28,7 @@ public class GuiAFSU extends GuiContainer{
 	public GuiAFSU(ContainerAFSU container) {
 		super(container);
 		
-		this.xSize = 176; //Is alright
+		this.xSize = 176;
 		this.ySize = 166;
 	    this.container = container;
 	    this.armorInv = StatCollector.translateToLocal("ic2.EUStorage.gui.info.armor");
@@ -44,18 +44,19 @@ public class GuiAFSU extends GuiContainer{
 
 	@Override
 	protected void drawGuiContainerForegroundLayer(int par1, int par2){
-		this.fontRendererObj.drawString(this.name, (this.xSize - this.fontRendererObj.getStringWidth(this.name)) / 2, 6, 4210752);
-	    //this.fontRendererObj.drawString(this.armorInv, 8, this.ySize - 126 + 3, 4210752);
+		/*this.fontRendererObj.drawString(this.name, (this.xSize - this.fontRendererObj.getStringWidth(this.name)) / 2, 8, 16448255);
+	    //this.fontRendererObj.drawString(this.armorInv, 8, this.ySize - 126 + 3, 16448255);
 
-	    this.fontRendererObj.drawString(this.level, 16, 25, 4210752);
+	    this.fontRendererObj.drawString(this.level, 16, 29, 16448255);
 	    int energy = (int)Math.min(((TileEntityAFSU)this.container.base).energy, ((TileEntityAFSU)this.container.base).maxStorage);
-	    this.fontRendererObj.drawString(" " + energy, 37, 35, 4210752);
-	    this.fontRendererObj.drawString("/" + ((TileEntityAFSU)this.container.base).maxStorage, 37, 45, 4210752);
+	    this.fontRendererObj.drawString(" " + energy, 37, 39, 16448255);
+	    this.fontRendererObj.drawString("/" + ((TileEntityAFSU)this.container.base).maxStorage, 37, 49, 16448255);
 
 	    String output = StatCollector.translateToLocalFormatted("ic2.EUStorage.gui.info.output", new Object[] { Integer.valueOf(((TileEntityAFSU)this.container.base).output) });
-	    this.fontRendererObj.drawString(output, 26, 60, 4210752);
+	    this.fontRendererObj.drawString(output, 26, 60, 16448255);
 
 	    GuiTooltiphelper.drawAreaTooltip(par1 - this.guiLeft, par2 - this.guiTop, ((TileEntityAFSU)this.container.base).getredstoneMode(), 10, 7, 29, 26);
+	    */
 	  }
 
 	@Override
@@ -64,11 +65,17 @@ public class GuiAFSU extends GuiContainer{
 	    this.mc.getTextureManager().bindTexture(background);
 	    int j = (this.width - this.xSize) / 2; //Good here
 	    int k = (this.height - this.ySize) / 2; //good here
-	    drawTexturedModalRect(j, k, 0, 0, this.xSize, this.ySize);//?
-	    if (((TileEntityAFSU)this.container.base).energy > 0.0D) {
-	    	int i1 = (int)(176.0F /*<-width of bar*/ * ((TileEntityAFSU)this.container.base).getChargeLevel());
-	     	drawTexturedModalRect(j + 8/*x of placement*/, k + 73 /*y of placement*/, 1/*x of bar*/, 251/*y of bar*/, i1 + 1, 5/*<-Height of bar*/);
-	    }
+	    //this.drawTexturedModalRect(j, k, 0, 0, this.xSize, this.ySize);//?
+	    /*if (((TileEntityAFSU)this.container.base).energy > 0.0D) {
+	    	float barwidth = 176.0F;
+	    	int xPlacementOfBar = 8;
+	    	int yPlacementOfBar = 73;
+	    	int xLocationOfBar = 0;
+	    	int yLocationOfBar = 251;
+	    	int barHeight = 5;
+	    	int i1 = (int)(barwidth * ((TileEntityAFSU)this.container.base).getChargeLevel());
+	     	drawTexturedModalRect(j + xPlacementOfBar, k + yPlacementOfBar, xLocationOfBar, yLocationOfBar, i1 + 1, barHeight);
+	    }*/
 	}
 
 	protected void actionPerformed(GuiButton guibutton){
