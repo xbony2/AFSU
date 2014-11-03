e5f6be28191f97a22b423212935c7c56efdf1275
xbony2 <xbony22@gmail.com>
2014-11-02 20:13:14 -0500

    More powerful config

--- src/main/java/xbony2/afsu/ConfigHandler.java
+++ src/main/java/xbony2/afsu/ConfigHandler.java
@@ -11,6 +11,31 @@ public class ConfigHandler {
 	private static final String[] usableValuesAFSU = {"default", "blue", "yellow", "black", "old"};
 	private static final String[] usableValuesAFB = {"default"};
 	
+	private static final String WARNING = "DO NOT CHANGE THIS IF YOU DONT KNOW WHAT YOU ARE DOING";
+	
+	public static int AFSUxSize;
+	public static int AFSUySize;
+	public static int AFSUGuiButtonid1;
+	public static int AFSUGuiButtonxpart;
+	public static int AFSUGuiButtonypart;
+	public static int AFSUGuiButtonw;
+	public static int AFSUGuiButtonh;
+	public static boolean AFSUGuiButtonDrawQuantity1;
+	public static int AFSUTextColor;
+	public static int AFSUyName;
+	public static int AFSUxLevel;
+	public static int AFSUyLevel;
+	public static int AFSUxEnergy;
+	public static int AFSUyEnergy;
+	public static int AFSUxEnergy2;
+	public static int AFSUyEnergy2;
+	public static int AFSUxEnergy3;
+	public static int AFSUyEnergy3;
+	public static int AFSUToolTipminx;
+	public static int AFSUToolTipminy;
+	public static int AFSUToolTipmaxx;
+	public static int AFSUToolTipmaxy;
+	
 	public static void init(File file){
 		Configuration config = new Configuration(file);
 		
@@ -18,6 +43,28 @@ public class ConfigHandler {
 		{
 			afsutexture = config.getString("AFSU Texture", "Textures", "default", "Changes the texture of the AFSU. Possible values: default, blue, yellow, black, old", usableValuesAFSU);
 			afbtexture = config.getString("AFB Texture", "Textures", "default", "Changes the texture of the AFB. Possible values: default", usableValuesAFB);
+			AFSUxSize = config.getInt("xSize", "GUI", 176, 0, Integer.MAX_VALUE, WARNING);
+			AFSUySize = config.getInt("ySize", "GUI", 166, 0, Integer.MAX_VALUE, WARNING);
+			AFSUGuiButtonid1 = config.getInt("GuiButtonid1", "GUI", 0, Integer.MIN_VALUE, Integer.MAX_VALUE, WARNING);
+			AFSUGuiButtonxpart = config.getInt("GuiButtonXPart", "GUI", 10, Integer.MIN_VALUE, Integer.MAX_VALUE, WARNING);
+			AFSUGuiButtonypart = config.getInt("GuiButtonYPart", "GUI", 7, Integer.MIN_VALUE, Integer.MAX_VALUE, WARNING);
+			AFSUGuiButtonw = config.getInt("GuiButtonW", "GUI", 20, 0, Integer.MAX_VALUE, WARNING);
+			AFSUGuiButtonh = config.getInt("GuiButtonH", "GUI", 20, 0, Integer.MAX_VALUE, WARNING);
+			AFSUGuiButtonDrawQuantity1 = config.getBoolean("GuiButtonDrawQuantity1", "GUI", true, WARNING);
+			AFSUTextColor = config.getInt("textColor", "GUI", 16448255, Integer.MIN_VALUE, Integer.MAX_VALUE, WARNING);
+			AFSUyName = config.getInt("yName", "GUI", 8, 0, Integer.MAX_VALUE, WARNING);
+			AFSUxLevel = config.getInt("xLevel", "GUI", 16, 0, Integer.MAX_VALUE, WARNING);
+			AFSUyLevel = config.getInt("yLevel", "GUI", 29, 0, Integer.MAX_VALUE, WARNING);
+			AFSUxEnergy = config.getInt("xEnergy", "GUI", 37, 0, Integer.MAX_VALUE, WARNING);
+			AFSUyEnergy = config.getInt("yEnergy", "GUI", 39, 0, Integer.MAX_VALUE, WARNING);
+			AFSUxEnergy2 = config.getInt("xEnergy2", "GUI", 37, 0, Integer.MAX_VALUE, WARNING);
+			AFSUyEnergy2 = config.getInt("yEnergy2", "GUI", 49, 0, Integer.MAX_VALUE, WARNING);
+			AFSUxEnergy3 = config.getInt("xEnergy3", "GUI", 26, 0, Integer.MAX_VALUE, WARNING);
+			AFSUyEnergy3 = config.getInt("yEnergy3", "GUI", 60, 0, Integer.MAX_VALUE, WARNING);
+			AFSUToolTipminx = config.getInt("toolTipMinX", "GUI", 10, 0, Integer.MAX_VALUE, WARNING);
+			AFSUToolTipminy = config.getInt("toolTipMinY", "GUI", 7, 0, Integer.MAX_VALUE, WARNING);
+			AFSUToolTipmaxx = config.getInt("toolTipMaxX", "GUI", 29, 0, Integer.MAX_VALUE, WARNING);
+			AFSUToolTipmaxy = config.getInt("toolTipMaxY", "GUI", 26, 0, Integer.MAX_VALUE, WARNING);
 		}
 		config.save();
 	}
--- src/main/java/xbony2/afsu/gui/GuiAFSU.java
+++ src/main/java/xbony2/afsu/gui/GuiAFSU.java
@@ -14,6 +14,7 @@ import net.minecraft.util.StatCollector;
 
 import org.lwjgl.opengl.GL11;
 
+import xbony2.afsu.ConfigHandler;
 import xbony2.afsu.container.ContainerAFSU;
 import xbony2.afsu.tileentity.TileEntityAFSU;
 
@@ -28,35 +29,40 @@ public class GuiAFSU extends GuiContainer{
 	public GuiAFSU(ContainerAFSU container) {
 		super(container);
 		
-		this.xSize = 176;
-		this.ySize = 166;
+		this.xSize = ConfigHandler.AFSUxSize;
+		this.ySize = ConfigHandler.AFSUySize;
 	    this.container = container;
 	    this.armorInv = StatCollector.translateToLocal("ic2.EUStorage.gui.info.armor");
 	    this.level = StatCollector.translateToLocal("ic2.EUStorage.gui.info.level");
-		this.name = "AFSU";
+		this.name = StatCollector.translateToLocal("tile.AFSU.name"); //Lazy on my part? Maybe. Works though!
 	}
 	
 	@Override
 	public void initGui(){
 	    super.initGui();
-	    this.buttonList.add(new GuiIconButton(0, (this.width - this.xSize) / 2 + 10, (this.height - this.ySize) / 2 + 7, 20, 20, new ItemStack(Items.redstone), true));
+	    this.buttonList.add(new GuiIconButton(ConfigHandler.AFSUGuiButtonid1, 
+	    		(this.width - this.xSize) / 2 + ConfigHandler.AFSUGuiButtonxpart, 
+	    		(this.height - this.ySize) / 2 + ConfigHandler.AFSUGuiButtonypart, 
+	    		ConfigHandler.AFSUGuiButtonw, ConfigHandler.AFSUGuiButtonh, new ItemStack(Items.redstone), 
+	    		ConfigHandler.AFSUGuiButtonDrawQuantity1));
 	}
 
 	@Override
 	protected void drawGuiContainerForegroundLayer(int par1, int par2){
-		/*this.fontRendererObj.drawString(this.name, (this.xSize - this.fontRendererObj.getStringWidth(this.name)) / 2, 8, 16448255);
-	    //this.fontRendererObj.drawString(this.armorInv, 8, this.ySize - 126 + 3, 16448255);
+		this.fontRendererObj.drawString(this.name, (this.xSize - this.fontRendererObj.getStringWidth(this.name)) / 2, ConfigHandler.AFSUyName, ConfigHandler.AFSUTextColor);
+	    //this.fontRendererObj.drawString(this.armorInv, 8, this.ySize - 126 + 3, ConfigHandler.AFSUTextColor);
 
-	    this.fontRendererObj.drawString(this.level, 16, 29, 16448255);
+	    this.fontRendererObj.drawString(this.level, ConfigHandler.AFSUxLevel, ConfigHandler.AFSUyLevel, ConfigHandler.AFSUTextColor);
 	    int energy = (int)Math.min(((TileEntityAFSU)this.container.base).energy, ((TileEntityAFSU)this.container.base).maxStorage);
-	    this.fontRendererObj.drawString(" " + energy, 37, 39, 16448255);
-	    this.fontRendererObj.drawString("/" + ((TileEntityAFSU)this.container.base).maxStorage, 37, 49, 16448255);
+	    this.fontRendererObj.drawString(" " + energy, ConfigHandler.AFSUxEnergy, ConfigHandler.AFSUyEnergy, ConfigHandler.AFSUTextColor);
+	    this.fontRendererObj.drawString("/" + ((TileEntityAFSU)this.container.base).maxStorage, ConfigHandler.AFSUxEnergy2, ConfigHandler.AFSUyEnergy2, ConfigHandler.AFSUTextColor);
 
 	    String output = StatCollector.translateToLocalFormatted("ic2.EUStorage.gui.info.output", new Object[] { Integer.valueOf(((TileEntityAFSU)this.container.base).output) });
-	    this.fontRendererObj.drawString(output, 26, 60, 16448255);
+	    this.fontRendererObj.drawString(output, ConfigHandler.AFSUxEnergy3, ConfigHandler.AFSUyEnergy3, ConfigHandler.AFSUTextColor);
 
-	    GuiTooltiphelper.drawAreaTooltip(par1 - this.guiLeft, par2 - this.guiTop, ((TileEntityAFSU)this.container.base).getredstoneMode(), 10, 7, 29, 26);
-	    */
+	    GuiTooltiphelper.drawAreaTooltip(par1 - this.guiLeft, par2 - this.guiTop, ((TileEntityAFSU)this.container.base).getredstoneMode(), 
+	    		ConfigHandler.AFSUToolTipminx, ConfigHandler.AFSUToolTipminy, ConfigHandler.AFSUToolTipmaxx, ConfigHandler.AFSUToolTipmaxy);
+	    
 	  }
 
 	@Override
@@ -65,8 +71,8 @@ public class GuiAFSU extends GuiContainer{
 	    this.mc.getTextureManager().bindTexture(background);
 	    int j = (this.width - this.xSize) / 2; //Good here
 	    int k = (this.height - this.ySize) / 2; //good here
-	    //this.drawTexturedModalRect(j, k, 0, 0, this.xSize, this.ySize);//?
-	    /*if (((TileEntityAFSU)this.container.base).energy > 0.0D) {
+	    this.drawTexturedModalRect(j, k, 0, 0, this.xSize, this.ySize);//?
+	    if (((TileEntityAFSU)this.container.base).energy > 0.0D) {
 	    	float barwidth = 176.0F;
 	    	int xPlacementOfBar = 8;
 	    	int yPlacementOfBar = 73;
@@ -75,7 +81,7 @@ public class GuiAFSU extends GuiContainer{
 	    	int barHeight = 5;
 	    	int i1 = (int)(barwidth * ((TileEntityAFSU)this.container.base).getChargeLevel());
 	     	drawTexturedModalRect(j + xPlacementOfBar, k + yPlacementOfBar, xLocationOfBar, yLocationOfBar, i1 + 1, barHeight);
-	    }*/
+	    }
 	}
 
 	protected void actionPerformed(GuiButton guibutton){
